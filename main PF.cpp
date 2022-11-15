3b88c49a40f23eb8314e1e517b918dda1ab42e31
Rim Chatti <chattiriim@gmail.com>
2022-11-15 12:57:10 +0100

    Avance

--- main PF.cpp
+++ main PF.cpp
@@ -2,6 +2,7 @@
 #include <vector>
 #include <algorithm>
 #include <fstream>
+#include <string> 
 using namespace std;
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
@@ -34,13 +35,63 @@ vector <vector<double>> load(){
 	return P;
 }
 
+bool same(vector<vector<double>> P,vector<vector<double>> Q){
+	for(int i = 1; i<P[0].size();i++){
+		if (P[i,0]!=Q[i,0] or P[i,1]!=Q[i,1] ){
+			cout << "The two polygons are different" << endl;
+			return false;
+		}
+	}
+	cout << "The two polygons are the same" << endl;
+	return true; 
+		
+}
+vector<vector<double>> clean(vector<vector<double>> P){
+		//secondToLast = myVector[myVector.size() - 2];
+		vector<double> m(P[0].size()-1);
+
+		for(int i = 1; i<P[0].size();i++){
+			m[i-1]=(P[0][i]-P[0][i-1])/(P[1][i]-P[1][i-1]);
+		}
+		
+		m.push_back(m[0]);
+		vector<double> X, Y;
+
+		for (int i=1;i<m.size();i++){
+			if(m[i]!=m[i-1]){
+				X.push_back(P[0][i]);
+				Y.push_back(P[1][i]);
+			}
+			
+		}
+		X.push_back(X[0]);
+		Y.push_back(Y[0]);
+		vector<vector<double>> PN;
+		PN.push_back(X);
+		PN.push_back(Y);
+	return PN;
+}
+
+void print(vector<vector<double>> P){
+		for(int j = 0; j < P[0].size(); j++){
+		cout << "P" << j << "= (" << P[0][j] << " , " << P[1][j] << ")" << endl;
+	}
+}
 
 int main() {
 	vector <vector<double>> P;   
 	P = load();
-	for(int j = 0; j < P[0].size(); j++){
-		cout << "P" << j << "= (" << P[0][j] << " , " << P[1][j] << ")" << endl;
-	}
+	cout << "Original polygon loaded" << endl;
+	print(P);
+	
+	P=clean(P);
+	cout << "Polygon after cleaning" << endl;
+	print(P);
+	vector <vector<double>> Q=P;
+	Q[0][0]=9;
+	cout << "Polygon for comparing" << endl;
+	print(Q);
+	same(P,Q);
 	
 	
 	/*
